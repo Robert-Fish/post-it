166cb97b49e953da3b1d46904caf318b2ea80676
Robert Fish <elasys63+github@gmail.com>
2021-06-01 00:22:54 +1000

    added searchbar to homepage

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,26 @@
-import Head from "next/head";
-import Image from "next/image";
+import { SearchIcon } from "@chakra-ui/icons";
+import { Heading, Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
+import { useState } from "react";
 
 export default function Home() {
-  return <div>test</div>;
+  const [searchTerm, setTerm] = useState("");
+
+  return (
+    <>
+      <Heading>Users</Heading>
+      <InputGroup mt={3} mb={3} data-testid="searchbar">
+        <InputLeftElement
+          pointerEvents="none"
+          children={<SearchIcon color="gray.300" />}
+        />
+        <Input
+          onChange={(e) => setTerm(e.target.value)}
+          value={searchTerm}
+          bg="white"
+          variant="outline"
+          placeholder="Search user by email or name..."
+        />
+      </InputGroup>
+    </>
+  );
 }
--- tests/pages/index.spec.js
+++ tests/pages/index.spec.js
@@ -9,7 +9,7 @@ import { act, fireEvent } from "@testing-library/react";
 
 describe("Home", () => {
   test("should render a grid of 10 users as cards", () => {
-    const { getAllByTestId } = await render(<Home />);
+    const { getAllByTestId } = render(<Home />);
     const profiles = getAllByTestId("profile");
     expect(profiles.length).toBe(10);
   });
