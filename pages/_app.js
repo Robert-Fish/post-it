37ddddfdc6e380fc5d821ca8f0a32394bca3d2a7
Robert Fish <elasys63+github@gmail.com>
2021-06-01 00:35:49 +1000

    added common page styles and axios config

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,39 @@
-import "../styles/globals.css";
 import { ChakraProvider } from "@chakra-ui/react";
+import axios from "axios";
+import { Global, css } from "@emotion/react";
+import Head from "next/head";
+
+axios.defaults.headers.common["app-id"] = process.env.NEXT_PUBLIC_DUMMY_APP_ID;
+axios.defaults.baseURL = "https://dummyapi.io/data/api/";
+
+const GlobalStyle = ({ children }) => {
+  return (
+    <>
+      <Head>
+        <title>Post It!</title>
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+      </Head>
+      <Global
+        styles={css`
+          html {
+            scroll-behavior: smooth;
+          }
+          #__next {
+            display: flex;
+            flex-direction: column;
+            min-height: 100vh;
+          }
+        `}
+      />
+      {children}
+    </>
+  );
+};
 
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider>
+      <GlobalStyle />
       <Component {...pageProps} />
     </ChakraProvider>
   );
